25bf2c352154ca54961424520ed3da5485f6eedb
Muhamad Farid Padilah <bbmfrd@gmail.com>
2020-06-04 14:25:34 +0700

    add layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DWS Nindya Karya',
+      title: 'Admin Pelaporan Bencana',
       theme: AppsTheme.nindyaTheme,
       home: SplashScreen(),
       routes: {
